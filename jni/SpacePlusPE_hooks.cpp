e4da9b856761e58299419631b00673ef77d7c9ea
TrinityDevelopers <trinitydevelopers4christ@gmail.com>
2015-09-13 15:09:32 -0500

    Woo got the pickaxe working perfectly!

--- jni/SpacePlusPE_hooks.cpp
+++ jni/SpacePlusPE_hooks.cpp
@@ -9,21 +9,18 @@
 #define LOG_TAG "SpacePlusPE"
 #define LOGI(...) ((void)__android_log_print(ANDROID_LOG_INFO, LOG_TAG, __VA_ARGS__))
 
-class Minecraft;
-
-static void (*_Minecraft$init)(Minecraft*, const std::string&);
-static void Minecraft$init(Minecraft* self, const std::string& string) {
-	_Minecraft$init(self, string);
-	
+static void (*_Item$initCreativeItems)();
+static void Item$initCreativeItems() {
 	SpacePlusCore::instance->preInit();
+	
+	_Item$initCreativeItems();
 }
 
 JNIEXPORT jint JNI_OnLoad(JavaVM* vm, void* reserved) {
 	void* handle = dlopen("libminecraftpe.so", RTLD_LAZY);
 	void* bl_handle = dlopen("libmcpelauncher.so", RTLD_LAZY);
-	void* Minecraft$init_symbol = dlsym(handle, "_ZN9Minecraft4initERKSs");
 	
-	MSHookFunction(Minecraft$init_symbol, (void*) &Minecraft$init, (void**) &_Minecraft$init);
+	MSHookFunction((void*) Item::initCreativeItems, (void*) &Item$initCreativeItems, (void**) &_Item$initCreativeItems);
 	
 	bl_armorRenders = (std::string**) dlsym(bl_handle, "bl_armorRenders");
 	
